7e2663db642e5b3f1d1c924d8ec0573f9fe6bbf9
angelo-james <angelojames.arriola@gmail.com>
2021-12-30 15:38:23 -0800

    remove assets and mockdata from gitignore

---
+++ src/constant/mockData.js
@@ -0,0 +1,75 @@
+import patentBred from "../assets/images/patent-bred.jpeg";
+import darkMocha from "../assets/images/dark-mocha.jpeg";
+import chicago from "../assets/images/chicago.jpeg";
+import fearless from "../assets/images/fearless.jpeg";
+import shadow from "../assets/images/shadow.jpeg";
+import universityBlue from "../assets/images/univeristy-blue.jpeg";
+import crimsonTint from "../assets/images/crimson-tint.jpeg";
+import royal from "../assets/images/royal.jpeg";
+import bredToe from "../assets/images/bred-toe.jpeg";
+import shatteredBackboard from "../assets/images/shattered-backboard.jpeg";
+
+const mockData = [
+  {
+    id: 1,
+    type: "Air Jordan 1",
+    name: "Patent Bred",
+    image: patentBred,
+  },
+  {
+    id: 2,
+    type: "Air Jordan 1",
+    name: "Dark Mocha",
+    image: darkMocha,
+  },
+  {
+    id: 3,
+    type: "Off White X Air Jordan 1",
+    name: "fearless",
+    image: chicago,
+  },
+  {
+    id: 4,
+    type: "Air Jordan 1",
+    name: "Fearless",
+    image: fearless,
+  },
+  {
+    id: 5,
+    type: "Air Jordan 1",
+    name: "Shadow",
+    image: shadow,
+  },
+  {
+    id: 6,
+    type: "Air Jordan 1",
+    name: "University Blue",
+    image: universityBlue,
+  },
+  {
+    id: 7,
+    type: "Air Jordan 1",
+    name: "Crimson Tint",
+    image: crimsonTint,
+  },
+  {
+    id: 8,
+    type: "Air Jordan 1",
+    name: "Royal",
+    image: royal,
+  },
+  {
+    id: 9,
+    type: "Air Jordan 1",
+    name: "Bred Toe",
+    image: bredToe,
+  },
+  {
+    id: 10,
+    type: "Air Jordan 1",
+    name: "Shattered Backboard",
+    image: shatteredBackboard,
+  },
+];
+
+export { mockData };
