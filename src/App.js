be748d26e20ee4a507a24437b54fe5ba12a8b3ce
angelo-james <angelojames.arriola@gmail.com>
2021-12-30 17:23:53 -0800

    create route for product detail page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import LandingPage from "./routes/landingPage/LandingPage";
+import ProductDetailPage from "./routes/ProductDetailPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path="/product-detail/:id" element={<ProductDetailPage />} />
         </Routes>
       </Layout>
     </Router>
